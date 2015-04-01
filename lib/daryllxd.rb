eb9b293ced2edee433127049979905b58f576334
Jedford Seculles <jfjade18@gmail.com>
2015-04-01 16:12:21 +0800

    rotate counter

--- lib/daryllxd.rb
+++ lib/daryllxd.rb
@@ -2,7 +2,8 @@ require "./lib/daryllxd/version"
 
 class String
   def darify!
-    downcase.scan(/([^aeiou]?[aeiou]([^aeiou](?![aeiou]))?)/).rotate.join.squeeze
+    new_str = downcase.scan(/([^aeiou]?[aeiou]([^aeiou](?![aeiou]))?)/)
+    new_str.rotate(new_str.length - 1).join.squeeze
   end
 end
 
